d9d6f43943d91358748eea9c40736b2fba7ba076
jimmyg <mailforjamesg@gmail.com>
2022-10-12 13:09:45 -0400

    HH config file update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
-      goerli: {}
-    }
+        goerli: {
+            url: GOERLI_RPC_URL,
+            // Add private keys as accounts
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
     solidity: "0.8.17",
 }
