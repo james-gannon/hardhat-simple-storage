e73496c0db560f4ab65d16e8547741d97ac9543f
jimmyg <mailforjamesg@gmail.com>
2022-10-24 16:29:48 -0400

    Added contract deployment verification

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,5 @@
 // imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
@@ -7,9 +7,45 @@ async function main() {
         "SimpleStorage"
     )
     console.log("Deploying contract...")
-    const SimpleStorage = await SimpleStorageFactory.deploy()
-    await SimpleStorage.deployed()
-    console.log(`Deployed contract to: ${SimpleStorage.address}`)
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    console.log("Hi there, face here.")
+    await simpleStorage.deployed()
+    console.log(`Deployed contract to: ${simpleStorage.address}`)
+    // Check the current network you are running on. Will not call verify function if delpoying to local Hardhat network.
+    if (process.env.ETHERSCAN_API_KEY) {
+        // Wait 6 block confirmations
+        console.log("Sit tight. Waiting for block confirmations...")
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current value is: ${currentValue}`)
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is: ${updatedValue}`)
+}
+
+// Automatically verify our contract after it deploys
+
+async function verify(contractAddress, args) {
+    // Verify through etherscan.io API
+    console.log("Verifying contract. Standby...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("Already Verified!")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 // main
